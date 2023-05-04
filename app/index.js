6bfd6787cef2984ecd7b2bda101ac04fdd37558c
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-04 22:19:34 +0300

    test

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 
 export default function Home() {
   return (
     <View>
       <Text>Home</Text>
+      <TextInput></TextInput>
     </View>
   );
 }
