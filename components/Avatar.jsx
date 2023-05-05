7aab17416a67752115fdeaf653c81f3c4ae6ab1d
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-06 00:06:44 +0300

    style avatar component

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { View, Image } from "react-native";
+import profile from "../assets/icons/profile.jpeg";
 
 export default function Avatar() {
-  return <View></View>;
+  return (
+    <View className=''>
+      <Image source={profile} className='rounded-full h-[30px] w-[30px]' />
+    </View>
+  );
 }
