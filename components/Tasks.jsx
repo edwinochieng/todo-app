3acc84d06b0d5f2293dfc584a3c07b6ff2064641
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-06 00:10:31 +0300

    tasks list component

---
+++ components/Tasks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Tasks() {
+  return (
+    <View className=''>
+      <Text className='uppercase text-xs font-semibold text-gray-700'>
+        Today's Tasks
+      </Text>
+    </View>
+  );
+}
