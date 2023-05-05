16111dffc17131437e7b8dfa2977f20b463a7ee6
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-06 00:10:04 +0300

    welcome message component

---
+++ components/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function Welcome() {
+  return (
+    <View className='mt-1 mb-2'>
+      <Text className='font-semibold text-2xl text-gray-700'>Hello Edwin</Text>
+    </View>
+  );
+}
