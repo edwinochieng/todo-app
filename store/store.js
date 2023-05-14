ac4945a4ae795c8d53b982e7bbf0cd3b095b3555
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-14 21:32:42 +0300

    persist store

--- store/store.js
+++ store/store.js
@@ -7,33 +7,38 @@ export const categories = [
   { name: "School" },
 ];
 
-const useStore = create((set, get) => ({
-  tasks: [],
+const useStore = create(
+  persist(
+    (set, get) => ({
+      tasks: [],
 
-  addTask: (newTask) => {
-    set({ tasks: [...get().tasks, newTask] });
-  },
+      addTask: (newTask) => {
+        set({ tasks: [...get().tasks, newTask] });
+      },
 
-  updateTask: (editedTask, taskId) => {
-    set(() => {
-      const tasks = get().tasks.map((task) => {
-        if (task.id === taskId) {
-          return { ...task, ...editedTask };
-        } else {
-          return task;
-        }
-      });
+      updateTask: (editedTask, taskId) => {
+        set(() => {
+          const tasks = get().tasks.map((task) => {
+            if (task.id === taskId) {
+              return { ...task, ...editedTask };
+            } else {
+              return task;
+            }
+          });
 
-      return { tasks };
-    });
-  },
+          return { tasks };
+        });
+      },
 
-  removeTask: (taskId) => {
-    set({ tasks: get().tasks.filter((item) => item.id !== taskId) });
-  },
-  clearStore: () => {
-    set({ tasks: [] });
-  },
-}));
+      removeTask: (taskId) => {
+        set({ tasks: get().tasks.filter((item) => item.id !== taskId) });
+      },
+      clearStore: () => {
+        set({ tasks: [] });
+      },
+    }),
+    { name: "todos" }
+  )
+);
 
 export default useStore;
