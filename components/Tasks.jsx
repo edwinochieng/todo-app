cd32e83eee94c04d76ee3eb1d8a0f8fcb62b5911
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-15 19:57:05 +0300

    display current day tasks only

--- components/Tasks.jsx
+++ components/Tasks.jsx
@@ -6,13 +6,18 @@ import ToDo from "./ToDo";
 export default function Tasks() {
   const tasks = useStore((state) => state.tasks);
 
+  const dateToday = new Date().toLocaleDateString();
+  const todayTasks = tasks.filter(
+    (task) => task.date.toLocaleDateString() === dateToday
+  );
+
   return (
     <View className=''>
       <Text className='uppercase text-xs font-semibold text-gray-700 mb-2'>
         Today's Tasks
       </Text>
       <FlatList
-        data={tasks}
+        data={todayTasks}
         renderItem={({ item }) => <ToDo id={item.id} title={item.title} />}
         keyExtractor={(item) => item.id}
       />
