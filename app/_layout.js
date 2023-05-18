73ea746cdee2a21f9e70a083e82d9624890740eb
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-05-18 23:13:30 +0300

    import fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 
 export default function Layout() {
+  const [fontsLoaded] = useFonts({
+    LatoRegular: require("../assets/fonts/Lato-Regular.ttf"),
+    LatoBold: require("../assets/fonts/Lato-Bold.ttf"),
+    LatoLight: require("../assets/fonts/Lato-Light.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack />;
 }
